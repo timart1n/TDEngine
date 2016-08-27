c98b70e9af21900e2a5a9e2f67daf934e62438ec
Tiago Martins Ribeiro <tiago.martins.ribeiro@cern.ch>
2016-08-27 11:39:42 +0200

    Changed name of entry point class from Main to TDEngine.

--- desktop/src/com/massivedisaster/tdengine/desktop/DesktopLauncher.java
+++ desktop/src/com/massivedisaster/tdengine/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.massivedisaster.tdengine.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.massivedisaster.tdengine.Main;
+import com.massivedisaster.tdengine.TDEngine;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+		new LwjglApplication(new TDEngine(), config);
 	}
 }
