46bd112673cfbc89dab7d6e24a4533f131cbad53
Tiago Martins Ribeiro <tiago.martins.ribeiro@cern.ch>
2016-08-27 11:33:26 +0200

    Added LibGDX project structure

---
+++ desktop/src/com/massivedisaster/tdengine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.massivedisaster.tdengine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.massivedisaster.tdengine.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
